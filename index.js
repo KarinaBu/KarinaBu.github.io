0dd27ab9cbc40cdf4b15bf4a541687a8ac30150a
KarinaBu <88528894+KarinaBu@users.noreply.github.com>
2021-08-26 14:59:21 +0300

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
 
   //Бургер-меню
 
@@ -12,55 +12,58 @@ window.addEventListener('DOMContentLoaded', function() {
 
   // Инпут-лупа 1920
 
-		document.querySelector('#search').addEventListener('click', function () {
-			document.querySelector('#input').classList.add('show');
-			document.querySelector('#search').focus();
-		});
+  document.querySelector('#search').addEventListener('click', function () {
+    document.querySelector('#input').classList.add('show');
+    document.querySelector('#search').focus();
+  });
 
-		document.querySelector('#input').addEventListener('blur', function () {
-			document.querySelector('#input').classList.remove('show');
-		})
+  document.querySelector('#input').addEventListener('blur', function () {
+    document.querySelector('#input').classList.remove('show');
+  })
 
-    document.getElementById('#reset').addEventListener('click', function () {
-			document.querySelector('#input').classList.remove('show');
-    })
+  document.getElementById('#reset').addEventListener('click', function () {
+    document.querySelector('#input').classList.remove('show');
+  })
 
 
-//Слайдер
+  //Слайдер
 
-const swiper = new Swiper('.swiper-container', {
-  /*autoplay: {
-    delay: 3000,
-  },*/
-  //slidesPerView: 'auto',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable:	'boolean',
-    type: 'bullets',
-  },
-});
+  const swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'boolean',
+      type: 'bullets',
+    },
+  });
 
   //Табы
-  document.querySelectorAll ('.how-we-work__item-link'). forEach(function(tabsItem) {
+  document.querySelectorAll('.how-we-work__item-link').forEach(function (tabsItem) {
     tabsItem.addEventListener('click', function (event) {
-       const path = event.currentTarget.dataset.path;
-       document.querySelectorAll('.tab-content').forEach (function (tabContent) {
+      const path = event.currentTarget.dataset.path;
+      document.querySelectorAll('.tab-content').forEach(function (tabContent) {
         tabContent.classList.remove('how-we-work__item-active');
-       })
-       document.querySelector(`[data-target="${path}"]`).classList.add('how-we-work__item-active');
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('how-we-work__item-active');
 
     })
-})
+  })
 
 
-//Аккордион
+  //Аккордион
 
-$( function() {
-  $( "#accordion" ).accordion({active : false, collapsible: true, header: '.question__line-text'});
-  $("h3.question__line-text").click(function() {
-  $(this).find('.plus_inactive').toggleClass('active');
-});
-});
+  $(function () {
+    $("#accordion").accordion({
+      active: false,
+      collapsible: true,
+      header: '.question__line-text'
+    });
+    $("h3.question__line-text").click(function () {
+      $(this).find('.plus_inactive').toggleClass('active');
+    });
+  });
 
 
 })
